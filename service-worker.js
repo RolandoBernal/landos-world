313b513a8f37a072b9a975f90dbbe1b1884cf3f0
Rolando Bernal <rolandobernal@Rolandos-MacBook-Air.local>
2026-08-23 20:01:00 -0500

    Add VFGT saved-game editing and seven-segment clock

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-const SW_VERSION = '2026-08-22-12';
+const SW_VERSION = '2026-08-24-1';
 const APP_CACHE = `landos-world-app-${SW_VERSION}`;
 const RUNTIME_CACHE = `landos-world-runtime-${SW_VERSION}`;
 const WEATHER_CACHE = `landos-world-weather-${SW_VERSION}`;
